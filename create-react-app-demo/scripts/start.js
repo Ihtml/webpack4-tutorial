b6ea8eb61695efc3a031d712b6c7184a2329f859
Ihtml <2483690020@qq.com>
2019-09-27 22:30:21 +0800

    docs(create-react-app): update webpack.config.js doc

--- create-react-app-demo/scripts/start.js
+++ create-react-app-demo/scripts/start.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// 开发环境配置
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
